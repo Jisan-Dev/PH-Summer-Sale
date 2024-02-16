9d000e157f104f73c727ef8cec57902e36820ab2
Jisun696 <istiakkashem35@gmail.com>
2024-02-16 18:02:24 +0600

    Add codes to add product title into the sidebar onclick

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const cards = document.querySelectorAll('.card');
+let counter = 0;
 
+for (let card of cards) {
+  card.addEventListener('click', () => {
+    const cardTitle = card.querySelector('h3').innerText;
+    const cardPrice = card.querySelector('span').innerText;
+    const titleContainer = document.getElementById('title-container');
+
+    const p = document.createElement('p');
+    p.innerText = `${++counter}. ${cardTitle}`;
+    titleContainer.appendChild(p);
+  });
+}

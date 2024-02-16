4b602bd7bbe5b921008810fb68fe3dd5d7e877e5
Jisun696 <istiakkashem35@gmail.com>
2024-02-16 23:20:14 +0600

    Add functionality to calculate totals

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const cards = document.querySelectorAll('.card');
 let counter = 0;
+// totalPrice
+let total = 0;
 
 for (let card of cards) {
   card.addEventListener('click', () => {
     const cardTitle = card.querySelector('h3').innerText;
-    const cardPrice = card.querySelector('span').innerText;
+    const cardTextPrice = card.querySelector('span').innerText.split(' ')[1];
+    const cardPrice = parseFloat(cardTextPrice);
     const titleContainer = document.getElementById('title-container');
+    const totalContainer = document.getElementById('total');
 
     const p = document.createElement('p');
     p.innerText = `${++counter}. ${cardTitle}`;
     titleContainer.appendChild(p);
+    // total price count
+    total += cardPrice;
+    totalContainer.innerText = total.toFixed(2);
   });
 }

aa0f0c6929c03a9b5dd2c1b0f3fb91a36b16c342
Jisun696 <istiakkashem35@gmail.com>
2024-02-16 23:49:55 +0600

    Add functionalities to calculate net total

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const cards = document.querySelectorAll('.card');
 let counter = 0;
 // totalPrice
 let total = 0;
+const discountAmount = document.getElementById('discountPrice');
+const couponInpValue = document.getElementById('input-field').value;
+const totalContainer = document.getElementById('total');
+const netTotal = document.getElementById('netTotal');
 
 for (let card of cards) {
   card.addEventListener('click', () => {
@@ -9,13 +13,21 @@ for (let card of cards) {
     const cardTextPrice = card.querySelector('span').innerText.split(' ')[1];
     const cardPrice = parseFloat(cardTextPrice);
     const titleContainer = document.getElementById('title-container');
-    const totalContainer = document.getElementById('total');
 
     const p = document.createElement('p');
     p.innerText = `${++counter}. ${cardTitle}`;
     titleContainer.appendChild(p);
     // total price count
     total += cardPrice;
-    totalContainer.innerText = total.toFixed(2);
+    const totalPrice = total.toFixed(2);
+    totalContainer.innerText = totalPrice;
+
+    if (totalContainer.innerText > 200) {
+      const discountAmountCalc = (totalPrice * 0.2).toFixed(2);
+      discountAmount.innerText = discountAmountCalc;
+      // total -= discountAmount;
+      // totalContainer.innerText = discountAmount;
+      netTotal.innerText = totalPrice - parseFloat(discountAmountCalc);
+    }
   });
 }
